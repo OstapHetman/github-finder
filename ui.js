e483103a48c77881df6c006f6f485c6b33db127f
OstapHetman <prickly4528@gmail.com>
2018-04-02 16:09:30 +0300

    fetch display repos

--- ui.js
+++ ui.js
@@ -32,6 +32,30 @@ class UI {
         `;
     }
 
+    // Show user repos
+    showRepos(repos) {
+        let output = '';
+        repos.forEach(repo => {
+            output += `
+                <div class="card card-body mb-2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                        </div>
+                        <div class="col-md-6">
+                            <span class="badge badge-primary mb-3">Stars: ${repo.stargazers_count}</span>
+                            <span class="badge badge-secondary mb-3">Watchers: ${repo.watchers_count}</span>
+                            <span class="badge badge-success mb-3">Forks: ${repo.forms_output}</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+
+        // Outup repos
+        document.getElementById('repos').innerHTML = output;
+    }
+
     // Show alert msg
     showAlert(msg, className) {
         // Clear any remaininf alerts
